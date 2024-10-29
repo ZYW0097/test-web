8735a72bfb1b5fd0ed419d0256c13d179acef471
ZYW0097 <zywei097@gmail.com>
2024-10-29 21:54:40 +0800

    Update database.js

--- database.js
+++ database.js
@@ -1,14 +1,17 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./bookings.db');
+const db = new sqlite3.Database('./database.db');
 
-// 創建表格
+// 創建 bookings 表
 db.serialize(() => {
-  db.run(`CREATE TABLE IF NOT EXISTS bookings (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    phone TEXT NOT NULL,
-    time TEXT NOT NULL
-  )`);
+    db.run(`CREATE TABLE IF NOT EXISTS bookings (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        phone TEXT,
+        time TEXT,
+        adults INTEGER,
+        children INTEGER,
+        childChairs INTEGER
+    )`);
 });
 
 module.exports = db;
