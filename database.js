05f83ada5c48d3336b8fcb2a92fad3ef78b64926
ZYW0097 <zywei097@gmail.com>
2024-10-29 23:26:06 +0800

    Update database.js

--- database.js
+++ database.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 // 替換為您的 MongoDB 連接字符串
 const mongoURI = 'mongodb+srv://zywei097:BFiolrXvvk3JMExn@test-web-db.ma336.mongodb.net/?retryWrites=true&w=majority&appName=test-web-db';
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+// 連接到 MongoDB
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected successfully'))
     .catch(err => console.error('Could not connect to MongoDB:', err));
 
+// 定義 Booking 模型的 Schema
 const bookingSchema = new mongoose.Schema({
     name: { type: String, required: true },
     phone: { type: String, required: true },
@@ -16,6 +18,8 @@ const bookingSchema = new mongoose.Schema({
     childChairs: { type: Number, default: 0 }
 });
 
+// 創建 Booking 模型
 const Booking = mongoose.model('Booking', bookingSchema);
 
+// 將 Booking 模型導出
 module.exports = Booking;
